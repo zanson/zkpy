9d7667e3e728d75c681a27d799bd7a60025243f6
luk <github@sachen.org>
2010-10-06 15:36:40 +0200

    removing unneeded variable

--- zkpy/__init__.py
+++ zkpy/__init__.py
@@ -28,13 +28,13 @@ def zk_retry_operation(operation,  retry_count = 10, retry_delay = 0.5):
         for attempt_count in range(1, retry_count):
             try:
                 return operation(*args, **kwargs)
-            except zookeeper.SessionExpiredException, e:
+            except zookeeper.SessionExpiredException:
                 logger.error('''Zookeeper session expired. Please clean up your state and start a new session and retry.''')
-                raise e
-            except zookeeper.ConnectionLossException, e:
+                raise
+            except zookeeper.ConnectionLossException:
                 if attempt_count >= retry_count:
                     logger.error('Retried operation for %d times. Giving up' % attempt_count)
-                    raise e
+                    raise
                 time.sleep(retry_delay)
         raise RetryOperationError('Could not execute %s. Retried for %d times' % (operation, retry_count))
     return wrapper
